e36e79f1abba4a9b7fed84be3bb752ff8bc6e5de
orgb95 <76708009+orgb95@users.noreply.github.com>
2021-05-26 08:25:08 -0600

    Add files via upload

---
+++ Practica/Practica/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtEdad_Enter(object sender, EventArgs e)
+        {
+
+            txtEdad.Text = "";
+        }
+
+        private void TxtEdad_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtEdad.Text))
+            {
+                txtEdad.Text = "Edad";
+            }
+        }
+
+        private void BtnAceptar_Click(object sender, EventArgs e)
+        {
+            int? edad = GetIntValue(txtEdad.Text);
+            if (edad == null)
+            {
+                return;
+            }
+            string categoria = cmbCategoria.SelectedItem.ToString();
+            string mensaje = "No coinciden la edad con la categoria";
+            switch (categoria)
+            {
+                case "Bebe":
+                    if (validarEdad(edad ?? 0, 0, 4))
+                    {
+                        mensaje = "Usted es un bebe";
+                    }
+                    break;
+                case "Niño":
+                    if (validarEdad(edad ?? 5, 5,11))
+                    {
+                        mensaje = "Usted es un Niño";
+                    }
+                    break;
+                case "Adolescente":
+                    if (validarEdad(edad ?? 12,12, 17))
+                    {
+                        mensaje = "Usted es un Adolescente";
+                    }
+                    break;
+                case "Joven":
+                    if (validarEdad(edad ?? 18, 18, 23))
+                    {
+                        mensaje = "Usted es un Joven";
+                    }
+                    break;
+                case "Adulto":
+                    if (validarEdad(edad ?? 24, 24, 60))
+                    {
+                        mensaje = "Usted es un Adulto";
+                    }
+                    break;
+                case "Adulto Mayor":
+                    if (validarEdad(edad ?? 61, 61, 120))
+                    {
+                        mensaje = "Usted es un Adulto Mayor";
+                    }
+                    break;
+                    
+            }
+            MessageBox.Show(mensaje);
+        }
+        private bool validarEdad(int edad ,int min, int max)
+        {
+            return (edad >= min && edad <= max);
+        }
+        private int? GetIntValue(String value)
+        {
+            if (int.TryParse(txtEdad.Text, out int edad))
+            {
+               return edad;
+            }
+          return null;
+
+        }
+
+        private void BtnTest_Click(object sender, EventArgs e)
+        {
+            object obj = 3;
+            long j = 4;
+            if(obj is int i)
+            {
+                MessageBox.Show($"{i} x {j} = {i * j}");
+            }
+            else
+            {
+                MessageBox.Show($"La variable obj ({obj.GetType()}) no es un entero");
+            }
+        }
+    }
+}
---
+++ Practica/Practica/FrmInterative.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Practica
+{
+    partial class FrmInterative
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumeros = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.txtResultados = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtNumeros
+            // 
+            this.txtNumeros.Location = new System.Drawing.Point(119, 42);
+            this.txtNumeros.Name = "txtNumeros";
+            this.txtNumeros.Size = new System.Drawing.Size(208, 20);
+            this.txtNumeros.TabIndex = 0;
+            this.txtNumeros.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox1_KeyDown);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(46, 45);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(49, 13);
+            this.label.TabIndex = 1;
+            this.label.Text = "Números";
+            // 
+            // txtResultados
+            // 
+            this.txtResultados.Location = new System.Drawing.Point(119, 97);
+            this.txtResultados.Multiline = true;
+            this.txtResultados.Name = "txtResultados";
+            this.txtResultados.Size = new System.Drawing.Size(208, 148);
+            this.txtResultados.TabIndex = 2;
+            // 
+            // FrmInterative
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 267);
+            this.Controls.Add(this.txtResultados);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.txtNumeros);
+            this.Name = "FrmInterative";
+            this.Text = "FrmInterative";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumeros;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox txtResultados;
+    }
+}
